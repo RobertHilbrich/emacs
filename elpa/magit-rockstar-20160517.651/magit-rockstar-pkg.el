cbb74ac818958d80e5d0b61e5566778d59bce890
Robert Hilbrich <Robert.Hilbrich@dlr.de>
2016-05-31 15:08:26 +0200

    more settings

---
+++ elpa/magit-rockstar-20160517.651/magit-rockstar-pkg.el
@@ -0,0 +1,1 @@
+(define-package "magit-rockstar" "20160517.651" "commit like a rockstar" '((dash "2.12.1") (magit "2.6.1")) :url "http://github.com/tarsius/magit-rockstar" :keywords '("convenience"))
