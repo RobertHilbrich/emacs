0aca1757751a161d2c90485e6d9e0cb3b5dc52b6
Robert Hilbrich <Robert.Hilbrich@dlr.de>
2017-03-14 08:33:18 +0100

    Added rtags and helm

--- elpa/irony-20170313.1437/irony-pkg.el
+++ elpa/irony-20170313.1437/irony-pkg.el
@@ -1,4 +1,4 @@
-(define-package "irony" "20170312.1515" "C/C++ minor mode powered by libclang"
+(define-package "irony" "20170313.1437" "C/C++ minor mode powered by libclang"
   '((cl-lib "0.5")
     (json "1.2"))
   :url "https://github.com/Sarcasm/irony-mode" :keywords
