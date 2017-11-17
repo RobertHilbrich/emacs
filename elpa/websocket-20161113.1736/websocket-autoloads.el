a7fb1f3f2f2f4ba2d683dd6e836a687c0199ce58
Robert Hilbrich <Robert.Hilbrich@dlr.de>
2017-11-17 13:55:32 +0100

    more additions

--- elpa/websocket-20161113.1736/websocket-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; websocket-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("websocket.el") (22572 6104 690829 688000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; websocket-autoloads.el ends here
